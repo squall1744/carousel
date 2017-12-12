aab35e5ab09e5ee806bf456cb8d0b671fe98c3dc
squall1744 <30652145+squall1744@users.noreply.github.com>
2017-12-12 13:58:01 +0800

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,74 +1,96 @@
-var $imgs = $('.carousel .img-ct li')
-var $imgCt = $('.img-ct')
-var $imgCon = $('.img-ct li img')
-var $pre = $('.carousel .pre')
-var $next = $('.carousel .next')
-var $bullet = $('.carousel .bullet li')
+function Carousel($cs) {
+  this.$imgCt = $cs.find('.img-ct')
+  this.$imgCon = $cs.find('.img-ct li')
+  this.$pre = $cs.find('.pre')
+  this.$next = $cs.find('.next')
+  this.$bullet = $cs.find('.bullet li')
+  this.count = this.$imgCon.length
+   
+  this.index = 0
+  this.isAnimate = false
+}
 
-var imgCount = $imgs.length
-var imgWidth = $imgCon.width()
-var index = 0
-var isAnimate = false
+Carousel.prototype.init = function() {
+  this.$first = this.$imgCon.first()
+  this.$last = this.$imgCon.last()
+  this.$imgCt.append(this.$first.clone())
+  this.$imgCt.prepend(this.$last.clone())
+  this.width = this.$imgCt.width(this.$imgCon.width() * (this.count+2))
+  this.$imgCt.css({
+    left: -this.$imgCon.width()
+  })
+  this.bind()
+  this.bullet()
+}
 
-$imgCt.append($imgs.first().clone())
-$imgCt.prepend($imgs.last().clone())
-$imgCt.width((imgCount + 2)*imgWidth)
-$imgCt.css({left: -imgWidth})
-
-setInterval(function(){
-  nextImg(1)
-}, 2000)
-
-$pre.on('click', function(){
-  preImg(1)
+Carousel.prototype.bind = function() {
+  var _this = this
+  this.$pre.on('click', function() {
+    _this.pre(1)
+  })
   
-})
-
-$next.on('click', function(){
-  nextImg(1)
-})
-
-$bullet.on('click', function(){
-  if($(this).index() > index) {
-    nextImg($(this).index()-index)
-  }else if($(this).index() < index){
-    preImg(index-$(this).index())
-  }
+  this.$next.on('click', function() {
+    _this.next(1)
+  })
   
-})
+}
 
-function preImg(len) {
-  if(isAnimate) {return}
-  isAnimate = true
-  $imgCt.animate({
-    left: '+=' + imgWidth * len
-  }, function(){
-    index -= len
-    if(index < 0) {
-      index = 3
-      $imgCt.css({left: -imgWidth*4})
+Carousel.prototype.pre = function(num) {
+  var _this = this
+  if(_this.isAnimate) return
+  _this.isAnimate = true
+  _this.$imgCt.animate({
+    left: '+=' + _this.$imgCon.width() * num
+  }, function() {
+    _this.isAnimate = false
+    _this.index -= num
+    _this.setBullet()
+    if(_this.index < 0){
+      _this.index = _this.count-1
+      _this.$imgCt.css({
+        left: -_this.$imgCon.width() * (_this.index+1)
+      })
+      _this.setBullet()
     }
-    setBullet()
-    isAnimate = false
   })
 }
 
-function nextImg(len) {
-  if(isAnimate) {return}
-  isAnimate = true
-  $imgCt.animate({
-    left: '-=' + imgWidth * len
-  }, function(){
-    index += len
-    if(index === imgCount) {
-      index = 0
-      $imgCt.css({left: -imgWidth})
+
+Carousel.prototype.next = function(num) {
+  var _this = this
+  if(_this.isAnimate) return
+  _this.isAnimate = true
+  _this.$imgCt.animate({
+    left: '-=' + _this.$imgCon.width() * num
+  }, function() {
+    _this.isAnimate = false
+    _this.index += num
+    _this.setBullet()
+    if(_this.index === _this.count){
+      _this.index = 0
+      _this.$imgCt.css({
+        left: -_this.$imgCon.width()
+      })
+      _this.setBullet()
     }
-    setBullet()
-    isAnimate = false
   })
 }
 
-function setBullet(){
- $bullet.removeClass('active').eq(index).addClass('active')
+Carousel.prototype.bullet = function() {
+  var _this = this
+  _this.$bullet.on('click', function(){
+    if($(this).index()- _this.index > 0){
+      _this.next($(this).index()- _this.index)
+    }else{
+      _this.pre(_this.index - $(this).index())
+    }
+  })
 }
+
+
+Carousel.prototype.setBullet = function() {
+  this.$bullet.removeClass('active').eq(this.index).addClass('active')
+}
+
+
+new Carousel($('.carousel').eq(0)).init()
